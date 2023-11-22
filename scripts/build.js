285dba2a13686e9b03082e5e5df8421efcecb88d
hiletmis <hiletmis@gmail.com>
2023-11-22 16:46:43 +0300

    Update react-icons-api3 version and add @api3/chains dependency

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,4 @@
+const chains = require('@api3/chains');
 const fs = require('fs/promises');
 const rimraf = require('rimraf');
 const svgr = require('@svgr/core').default;
@@ -20,9 +21,11 @@ async function transformSVGtoJSX(file, componentName, format) {
         },
         { componentName }
     );
-    const { code } = await babel.transformAsync(svgReactContent, {
+
+    let { code } = await babel.transformAsync(svgReactContent, {
         presets: [['@babel/preset-react', { useBuiltIns: true }]],
     });
+
 
     if (format === 'esm') {
         return code;
@@ -54,30 +57,75 @@ function indexFileContent(files, format, includeExtension = true) {
 }
 
 async function buildIcons(format = 'esm') {
-    let outDir = outputPath;
-    if (format === 'esm') {
-        outDir = `${outputPath}/esm`;
-    } else {
-        outDir = `${outputPath}/cjs`;
-    }
+    let outDir = `${outputPath}/${format}`;
 
     await fs.mkdir(outDir, { recursive: true });
 
     const files = await fs.readdir('./optimized', 'utf-8');
 
-    await Promise.all(
-        files.flatMap(async (fileName) => {
-            const componentName = `${camelcase(fileName.replace(/.svg/, ''), {
-                pascalCase: true,
-            })}Icon`;
-            const content = await transformSVGtoJSX(fileName, componentName, format);
-            const types = `import * as React from 'react';\ndeclare function ${componentName}(props: React.SVGProps<SVGSVGElement>, symbol: string): JSX.Element;\nexport default ${componentName};\n`;
+    chains.CHAINS.forEach(async (chain) => {
+        const file = files.find(file => file.includes(`Chain${chain.id}`))
+        let fileName = file;
 
-            // console.log(`- Creating file: ${componentName}.js`);
-            await fs.writeFile(`${outDir}/${componentName}.js`, content, 'utf-8');
-            await fs.writeFile(`${outDir}/${componentName}.d.ts`, types, 'utf-8');
-        })
-    );
+        if (!fileName) {
+            console.log(`Chain ${chain.id} not found`)
+            return
+        }
+
+        const componentName = `${camelcase(fileName.replace(/.svg/, ''), {
+            pascalCase: true,
+        })}Icon`;
+        const content = await transformSVGtoJSX(fileName, componentName, format);
+        const types = `import * as React from 'react';\ndeclare function ${componentName}(props: React.SVGProps<SVGSVGElement>): JSX.Element;\nexport default ${componentName};\n`;
+
+        // console.log(`- Creating file: ${componentName}.js`);
+        await fs.writeFile(`${outDir}/${componentName}.js`, content, 'utf-8');
+        await fs.writeFile(`${outDir}/${componentName}.d.ts`, types, 'utf-8');
+
+    })
+
+    const types = `import * as React from 'react';\ndeclare function Chains(props: React.SVGProps<SVGSVGElement>): JSX.Element;\nexport default Chains;\n`;
+    await fs.writeFile(
+        `${outDir}/Chains.d.ts`,
+        types,
+        'utf-8'
+    )
+    let { code } = await babel.transformAsync(`
+        import * as React from "react";
+        import Chain43113Icon from './Chain43113Icon';
+        import Chain11155111Icon from './Chain11155111Icon';
+        
+        function Chains(props) {
+            switch (props.id) {
+                case "43113":
+                    return <Chain43113Icon {...props} />;
+                case "11155111":
+                    return <Chain11155111Icon {...props} />;
+                default:
+                    return <Chain43113Icon {...props} />;
+            }
+        }
+
+        export default Chains;
+    `
+        , {
+            presets: [['@babel/preset-react', { useBuiltIns: true }]],
+        });
+
+    if (format === 'cjs') {
+        code = code
+            .replace(
+                'import * as React from "react";',
+                'const React = require("react");'
+            )
+            .replace('export default', 'module.exports =');
+    }
+
+    await fs.writeFile(
+        `${outDir}/Chains.js`,
+        code,
+        'utf-8'
+    )
 
     console.log('- Creating file: index.js');
     await fs.writeFile(
@@ -91,6 +139,9 @@ async function buildIcons(format = 'esm') {
         'utf-8'
     );
 }
+
+
+
 
 (function main() {
     console.log('🏗 Building icon package...');
